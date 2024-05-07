b11e0e5380de340843bfb4d5f49a497793017b0a
Lary_Stef <timurkotov1999@gmail.com>
2024-05-07 12:51:32 +0300

    fix: delete unnecessary

--- TrafficLights/SimulationForm.cs
+++ TrafficLights/SimulationForm.cs
@@ -1,16 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Security.Policy;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace TrafficLights
 {
